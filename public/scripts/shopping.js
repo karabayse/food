241ffd3445c882ab8de117434705dda38aab6896
Kara Bayse <karabayse@gmail.com>
2018-05-10 16:56:03 -0500

    added function to add item

--- public/scripts/shopping.js
+++ public/scripts/shopping.js
@@ -1,5 +1,11 @@
 console.log('in shopping.js');
 
-function addItem() {
-  
+// Event Listener
+document.querySelector('shopping-list').addEventListener('click', addItem);
+
+function addItem(e) {
+  console.log('in function addItem');
+  let newItem = document.querySelector('shopping-list').value;
+
+  document.querySelector('#itemList').innerHTML = listItem;
 }
