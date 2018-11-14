5697dbf056d8f79493531f3331cf6d6eae409f0e
Kara Bayse <karabayse@gmail.com>
2018-11-14 14:59:52 -0600

    added function to complete conversions

--- public/scripts/conversionsController.js
+++ public/scripts/conversionsController.js
@@ -4,6 +4,16 @@ myApp.controller('ConversionsController', function(ConversionsService) {
   console.log('in ConversionsController');
   let vm = this;
 
-
+  vm.convertInput = function() {
+    console.log('in convertInput function');
+    let conversionObject = {
+      input: vm.conversionInput
+    }; // end conversionsObject
+    console.log('conversionObject:', conversionObject);
+    ShoppingService.convertInput(conversionObject).then(function() {
+      sweetAlert("Item Added!");
+      vm.conversionInput = '';
+    }); // end ConversionsService.addItem
+  };
 
 });
